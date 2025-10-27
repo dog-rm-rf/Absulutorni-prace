3c5ff11f2d82afc728637b5381968253e8f10541
vojtasek <nno.spacee@gmail.com>
2025-10-27 18:10:46 +0000

    27.10.2025 19:10

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 # -*- coding: utf-8 -*-
-import sqlite3
 import sys
 import io
+import mysql.connector
 from datetime import datetime 
 from models.task import Task
 from models.all_tasks import All_tasks
 
+
 # Fix Windows console encoding for Czech characters
 sys.stdout.reconfigure(encoding='utf-8')
+
+#Create connection
+connection = mysql.connector.connect(
+    host='localhost',        # MySQL is running on your PC
+    user='your_username',    # MySQL user account
+    password='your_password', # MySQL password
+    database='task_manager'  # Name of your database
+)
+
+cursor = connection.cursor()  # This lets you send SQL commands
+
+
+
 # global variables
 nAll_tasks = All_tasks()
 output = None
+
 print("Hello welcome to 12 week app")
+#the progarm
 while (output != 0):
     
     print("If you want add task press 1\n" + 
